aef8fb0943325a75c19864401527e65677eb7244
serge Kenne <kenneserge77@gmail.com>
2023-04-26 18:58:46 +0300

    made desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-import './_vendor';
-import vars from './_vars';
-import './_functions';
-import './_components';
+// import './_vendor';
+// import vars from './_vars';
+// import './_functions';
+// import './_components';
+
+
+//  Initialize Swiper
+let swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
