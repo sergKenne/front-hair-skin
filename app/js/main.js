cec5b45098124111bc645d100a0ecdf7d2094192
serge Kenne <kenneserge77@gmail.com>
2023-04-27 18:53:16 +0300

    made slider

--- app/js/main.js
+++ app/js/main.js
@@ -9,15 +9,91 @@ var __webpack_exports__ = {};
 // import './_components';
 
 //  Initialize Swiper
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".courseSwiper", {
   pagination: {
-    el: ".swiper-pagination"
+    el: ".swiper-pagination",
+    clickable: true
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+  breakpoints: {
+    1280: {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction"
+      }
+    },
+    1600: {}
   }
 });
+let swiper2 = new Swiper(".reviewSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1.4,
+      spaceBetween: 20
+    },
+    1280: {
+      slidesPerView: 2.4,
+      spaceBetween: 20,
+      //slidesPerView:"auto",
+      //centeredSlides: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction"
+      }
+    },
+    1600: {}
+  }
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  loop: true,
+  loopedSlides: 4,
+  breakpoints: {
+    768: {
+      slidesPerView: 1.4,
+      spaceBetween: 60
+    },
+    1280: {
+      //slidesPerView: 1.8,
+    },
+    1600: {}
+  }
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 20,
+  //centeredSlides: true,
+  slidesPerView: 'auto',
+  //touchRatio: 0.2,
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 4,
+  breakpoints: {
+    768: {},
+    1280: {
+      direction: "vertical"
+    },
+    1600: {}
+  }
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
